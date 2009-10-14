667a9f6c78a5611ace8eacb553226640dd8cc5fa
Matthew P. Schouppe <devnull@localhost>
2009-10-13 22:24:34 -0400

    Created user session migration, modified home controller to count incorrect login attempts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,20 +8,25 @@ class HomeController < ApplicationController
   end
 
   def login
+    @count = 0 if @count == nil
+    logger.debug @count.to_s
     loginid = params[:home]
     c = Contact.find_by_strFirstname_and_strLastname(loginid[:strFirstname], loginid[:strLastname])
 
     if (c == nil)
       flash[:notice] = "The user cannot be found."
-      @count == 1 unless @count > 1
-      redirect_to :controller=>:login, :action=>:create if @count < 3
       @count += 1
+      if @count > 3
+        redirect_to :controller=>:login, :action=>:create
+      else
+        redirect_to :action=>:index
+      end
       return
     else
       flash[:notice] = "Found our guy!"
     end
 
-    redirect_to :action=>'index'
-
+    logger.debug 'should redirect'
+    redirect_to :action=>:index
   end
 end
