6a83d5dbddf99e8df5a9f95cbfbe09e88e790b06
Matthew Schouppe <Matthew.Schouppe@nationalcity.com>
2009-12-20 09:08:18 -0500

    Updated the login/show template to use a helper, which in turn uses a partial to render any assoicated work orders for a particular client

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,2 +1,19 @@
 module LoginHelper
+  def displayworkorders(workorders, &block)
+    if (workorders.length == 0)
+      content_tag(:h3, 'There are no workorders to display.')
+    else
+      concat "<table id='workorders'><tr>", block.binding
+      concat '<th>Action</th>', block.binding
+      concat '<th>Description</th>', block.binding
+      concat '<th>Date</th>', block.binding
+      concat '</tr>', block.binding
+
+      for order in workorders do
+        yield(order)
+      end
+
+      concat '</table>', block.binding
+    end
+  end
 end
