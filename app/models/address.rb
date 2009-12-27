318704dc10fb5556ba0dfefea0aa8920658555da
Xanothos <xanothos@gmail.com>
2009-12-27 09:21:47 -0500

    Updated address tests and fixture data

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,10 @@
 class Address < ActiveRecord::Base
-  validates_presence_of :addresstype
+  validates_presence_of :address_type
+  validates_presence_of :contact_id
 
-  belongs_to :addresstype, 
-    :class_name => :Listitem,
-    :foreign_key => :address_type_id
+  belongs_to :address_type,
+    :class_name => "Listitem"
+    #:foreign_key => "address_type_id"
 
-  belongs_to :contacts
+  belongs_to :Contact
 end
--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,9 +1,39 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  test 'address type cannot be nil' do
+  test 'validate presence of address_type' do
     a = Address.new
-    assert !a.save, 'Address was saved successfully'
-    assert a.errors.invalid?(:addresstype)
+    a.save
+
+    # the save should have aborted leaving us with an unsaved record (or new record)
+    assert a.new_record?, 'Address was saved unexpectedly!'
+    assert a.errors.invalid?(:address_type)
   end
+
+  test 'validate presence of contact_id' do
+    a = Address.new
+    a.save
+
+    # the save should have aborted leaving us with an unsaved record (or new record)
+    assert a.new_record?, 'Address was saved unexpectedly!'
+    assert a.errors.invalid?(:contact_id)
+  end
+
+  test 'proper address creation' do
+    # the corresponsing list item must exist, so find it or create it
+    l = Listitem.find_or_create_by_strListName_and_strListValue('address_type', 'home')
+
+    a = Address.create(
+        :address1 => '123 Anywhere Street',
+        :address2 => nil,
+        :city => 'Springfield',
+        :strState => 'CA',
+        :postal_code => '90210',
+        :address_type_id => l.id,
+        :contact_id => 1)
+
+    assert a.new_record? == false, 'Address was not saved as expected!'
+    
+  end
+
 end
