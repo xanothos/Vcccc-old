3eb0329ade78a1d8a07e6b9b83835c55cf0eaaed
Xanothos <xanothos@gmail.com>
2009-12-27 09:25:00 -0500

    Updated Email model to invoke belongs_to correctly (using strings not symbols for :class_name and :foreign_key), and corrected the Contact association

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,10 +1,10 @@
 class Email < ActiveRecord::Base
-  validates_presence_of :emailtype
+  validates_presence_of :email_type
 
-  belongs_to :contacts
+  belongs_to :Contact
 
-  belongs_to :emailtype,
-    :class_name=>'Listitem',
-    :foreign_key=>'email_type_id'
+  belongs_to :email_type,
+    :class_name=>'Listitem'
+    #:foreign_key=>'email_type_id'
 
 end
